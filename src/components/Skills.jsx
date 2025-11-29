0689bbb656e3d1b06411274271be53c40e8d1891
unknown <amadip272@gmail.com>
2025-11-29 23:24:15 +0100

    skills section updated

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -8,51 +8,71 @@ import img5 from '../assets/Sass.png'
 import img6 from '../assets/Figma.png'
 import img7 from '../assets/Git.png'
 
-// import img1 from '..assets/'
-
-
 const Skills = () => {
   return (
     <>
-      <div id="skills" className='bg-gray-900 min-h-1/2 px-6 py-6'>
-        {/* HEADING  */}
-         <div className='flex items-center justify-center text-3xl font-bold'>
-            <h2 className='text-gray-200'>My <span className='text-blue-300'>Skills</span></h2>
-         </div>
-           {/* ICONS  */}
-         <div className='flex items-center justify-center pt-16 pl-5'>
-            <ul className='flex align-center gap-12 pb-4'>
-              <li className='w-16 lg:w-[150px] text-gray-200 font-poppins'>
-                <img src={img} alt="" /> <span className='flex justify-center'> Javascript </span> </li>
+      <div id="skills" className="bg-gray-900 px-6 py-10">
+        
+        {/* HEADING */}
+        <div className="flex items-center justify-center text-3xl font-bold">
+          <h2 className="text-gray-200">
+            My <span className="text-blue-300">Skills</span>
+          </h2>
+        </div>
 
-              <li className='w-16 pt-2 lg:w-[180px] text-gray-200 font-poppins'>
-                <img src={img1} alt="" /> <span className='flex justify-center'> Typescript</span> </li>
+        {/* ICONS ROW 1 */}
+        <div className="flex items-center justify-center pt-10">
+          <ul className="flex flex-wrap justify-center gap-6 md:gap-12">
+            <li className="w-14 md:w-20 lg:w-[150px] text-gray-200 font-poppins flex flex-col items-center">
+              <img src={img} alt="" />
+              <span className="mt-1">Javascript</span>
+            </li>
 
-              <li className='w-16 lg:w-[150px] text-gray-200 font-poppins'>
-                <img src={img2} alt="" /> <span className='flex justify-center'>React </span></li>
+            <li className="w-14 md:w-20 lg:w-[180px] text-gray-200 font-poppins flex flex-col items-center">
+              <img src={img1} alt="" />
+              <span className="mt-1">Typescript</span>
+            </li>
 
-              <li className='w-16 lg:w-[150px] text-gray-200 font-poppins'>
-                <img src={img3} alt="" /> <span className='flex justify-center'>Next js</span></li>
-            </ul>
-         </div>
-         <div className='flex items-center justify-center'>
-            <ul className='flex align-center gap-12 pt-4'>
-              <li className='w-16 pt-4 lg:w-[150px] text-gray-200 font-poppins'>
-                <img src={img4} alt="" /><span className='flex justify-center'>Tailwind</span></li>
+            <li className="w-14 md:w-20 lg:w-[150px] text-gray-200 font-poppins flex flex-col items-center">
+              <img src={img2} alt="" />
+              <span className="mt-1">React</span>
+            </li>
 
-              <li className='w-16 lg:w-[150px] text-gray-200 font-poppins' >
-                <img src={img5} alt="" /> <span className='flex justify-center'>Sass/Scss</span></li>
+            <li className="w-14 md:w-20 lg:w-[150px] text-gray-200 font-poppins flex flex-col items-center">
+              <img src={img3} alt="" />
+              <span className="mt-1">Next JS</span>
+            </li>
+          </ul>
+        </div>
 
-              <li className='w-16 lg:w-[150px] text-gray-200 font-poppins'>
-                <img src={img6} alt="" /><span className='flex justify-center'>Figma</span></li>
+        {/* ICONS ROW 2 */}
+        <div className="flex items-center justify-center pt-6">
+          <ul className="flex flex-wrap justify-center gap-6 md:gap-12">
+            <li className="w-14 md:w-20 lg:w-[150px] text-gray-200 font-poppins flex flex-col items-center">
+              <img src={img4} alt="" />
+              <span className="mt-1">Tailwind</span>
+            </li>
 
-              <li className='w-16 lg:w-[150px] text-gray-200 font-poppins'>
-                <img src={img7} alt="" /> <span className='flex justify-center'> Git/Version control</span></li>
-            </ul>
-         </div>
+            <li className="w-14 md:w-20 lg:w-[150px] text-gray-200 font-poppins flex flex-col items-center">
+              <img src={img5} alt="" />
+              <span className="mt-1">Sass/Scss</span>
+            </li>
+
+            <li className="w-14 md:w-20 lg:w-[150px] text-gray-200 font-poppins flex flex-col items-center">
+              <img src={img6} alt="" />
+              <span className="mt-1">Figma</span>
+            </li>
+
+            <li className="w-14 md:w-20 lg:w-[150px] text-gray-200 font-poppins flex flex-col items-center">
+              <img src={img7} alt="" />
+              <span className="mt-1">Git / Version Control</span>
+            </li>
+          </ul>
+        </div>
+
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Skills
+export default Skills;
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/David",
+  base: "/DAVID_PETER",
   server:{
     port: 3000,
   }
